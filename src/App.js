439a682e38adcc2daa0d9cf00033b0f7db0cc969
Francielli Viana <108751170+FrancielliMota@users.noreply.github.com>
2024-05-07 20:21:21 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TypingSign from './TypingSign';
+import DateTimePicker from './components/DateTimePicker';
+
+const App = () => {
+  return (
+    <div>
+      <DateTimePicker />
+      <TypingSign />
+
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DateTimePicker from './components/DateTimePicker'; 
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DateTimePicker />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
