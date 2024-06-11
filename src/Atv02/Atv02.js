cd0c99be89d7079be545500809dffce8ef5b03d0
Francielli Viana <108751170+FrancielliMota@users.noreply.github.com>
2024-06-11 19:35:29 -0300

    Atividades em React realizadas no 2º Bimestre

---
+++ src/Atv02/Atv02.js
@@ -0,0 +1,18 @@
+import Header from '../Header';
+import Footer from '../Footer';
+import ContadorDePessoas from './ContadorDePessoas';
+import '../style.css';
+
+export default function Atv02() {
+    return (
+
+        
+        <>
+           
+            <Header />
+                <ContadorDePessoas/>
+            <Footer />
+        
+        </>
+    );
+}
